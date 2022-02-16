e4a982c6f29feaa0e73be899e51f8ab9702bd1c2
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2022-02-16 18:18:19 +0530

    Second Commit

---
+++ Assets/Scripts/Backbutton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backbutton : MonoBehaviour
+{
+    public void goBack()
+    {
+        Debug.Log("Go Back");
+        SceneManager.LoadScene("Main");
+    }
+}
--- Assets/Scripts/Levelmanager.cs
+++ Assets/Scripts/Levelmanager.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Levelmanager : MonoBehaviour
 {
+    [SerializeField]
+    private Button[] _levelBtn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0;i<_levelBtn.Length;i++)
+        {
+            _levelBtn[i].onClick.AddListener(levelClick);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void levelClick()
+    {
+        Debug.Log("clicked");
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        string str = EventSystem.current.currentSelectedGameObject.name;
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(str);
+    }
+    private void goBack()
+    {
+        Debug.Log("Go Back");
     }
 }
